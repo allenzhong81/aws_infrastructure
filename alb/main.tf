b9e2396aa84d4a6205f59e607fa7588427c70030
Allen.Zhong <allen.zhong@myob.com>
2019-03-11 08:49:36 +1300

    Change port to 80 and 443

--- alb/main.tf
+++ alb/main.tf
@@ -9,10 +9,17 @@ resource "aws_security_group" "public_alb_sg" {
   vpc_id = "${var.vpc_id}"
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    protocol    = -1
-    from_port = 0
-    to_port   = 0
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
